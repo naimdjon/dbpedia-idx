c85a1d092e4bf86ab0908bd2e6e719415c8e9b5c
naimdjon <naimdjon@gmail.com>
2015-02-05 08:16:20 +0100

    mongo helper

---
+++ src/main/java/no/westerdals/dbpedia_idx/MongoHelper.java
@@ -0,0 +1,28 @@
+package no.westerdals.dbpedia_idx;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+import java.net.UnknownHostException;
+
+public class MongoHelper {
+
+    private final DBCollection labels;
+
+    public MongoHelper() {
+        try {
+            final MongoClient mongoClient = new MongoClient();
+            final DB db = mongoClient.getDB("dbpedia");
+            this.labels = db.getCollection("labels");
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertTripleLabel(final Triple triple) {
+        labels.insert(new BasicDBObject("s",triple.subject).append("o",triple.object));
+    }
+}
