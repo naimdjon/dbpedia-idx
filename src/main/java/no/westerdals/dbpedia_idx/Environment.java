4506d7c3fb54a15ae40ade1316d3bb55694f1c97
naimdjon <naimdjon@gmail.com>
2015-02-22 13:21:33 +0100

    Environment.

--- src/main/java/no/westerdals/dbpedia_idx/Environment.java
+++ src/main/java/no/westerdals/dbpedia_idx/Environment.java
@@ -1,9 +1,30 @@
 package no.westerdals.dbpedia_idx;
 
+import java.io.File;
+
 public class Environment {
 
     public static boolean isLinux() {
         return System.getProperty("os.name").contains("Linux");
     }
 
+    public static String getIndexDir() {
+        String indexDir="/Users/taknai/NEEL_LUCENE_INDEX";
+        if (Environment.isLinux()) {
+            indexDir=indexDir.replaceFirst("Users","home").replaceFirst("takhirov","taknai");
+        }
+        File f=new File(indexDir);
+        if (!f.exists()) {
+            f.mkdir();
+        }
+        return indexDir;
+    }
+
+    public static String getInputFile(final String name) {
+        String inputFile="/Users/takhirov/Downloads/"+name+"_en.nt";
+        if (Environment.isLinux()) {
+            inputFile="/home/taknai/Downloads/"+name+"_en.nt";
+        }
+        return inputFile;
+    }
 }
