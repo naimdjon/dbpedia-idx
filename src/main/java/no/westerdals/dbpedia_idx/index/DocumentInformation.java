095bb91f95678cbe95cbd55d7ea58a52bfb8c5e1
naimdjon <naimdjon@gmail.com>
2015-02-28 18:34:47 +0100

    DocumentInformation for use to visit each document in the index.

---
+++ src/main/java/no/westerdals/dbpedia_idx/index/DocumentInformation.java
@@ -0,0 +1,11 @@
+package no.westerdals.dbpedia_idx.index;
+
+public class DocumentInformation {
+    final String id;
+    final int documentNumber;
+
+    public DocumentInformation(String id, int documentNumber) {
+        this.id = id;
+        this.documentNumber = documentNumber;
+    }
+}
