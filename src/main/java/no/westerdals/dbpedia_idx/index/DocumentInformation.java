774b4346fc9e327f4ca5538104740ada6798a4b2
naimdjon <naimdjon@gmail.com>
2015-03-02 21:11:07 +0100

    DocumentInformation changes.

--- src/main/java/no/westerdals/dbpedia_idx/index/DocumentInformation.java
+++ src/main/java/no/westerdals/dbpedia_idx/index/DocumentInformation.java
@@ -1,11 +1,11 @@
 package no.westerdals.dbpedia_idx.index;
 
 public class DocumentInformation {
-    final String id;
+    final String md5id;
     final int documentNumber;
 
-    public DocumentInformation(String id, int documentNumber) {
-        this.id = id;
+    public DocumentInformation(String md5id, int documentNumber) {
+        this.md5id = md5id;
         this.documentNumber = documentNumber;
     }
 }
