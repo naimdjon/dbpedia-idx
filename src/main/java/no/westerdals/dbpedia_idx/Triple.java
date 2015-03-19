e97e3c7696550c5cdf89d32dabf5396972b76d08
naimdjon <naimdjon@gmail.com>
2015-03-19 21:54:57 +0100

    added MD5 title to doc.

--- src/main/java/no/westerdals/dbpedia_idx/Triple.java
+++ src/main/java/no/westerdals/dbpedia_idx/Triple.java
@@ -3,11 +3,13 @@ package no.westerdals.dbpedia_idx;
 public final class Triple {
 
     public final String subject, predicate, property;
+    private final String MD5Title;
 
     public Triple(String subject, String predicate, String property) {
         this.subject = subject;
         this.predicate = predicate;
         this.property = property;
+        this.MD5Title=MD5.hash(this.subject);
     }
 
     @Override
@@ -17,5 +19,9 @@ public final class Triple {
                 ", predicate='" + predicate + '\'' +
                 ", property='" + property + '\'' +
                 '}';
+    }
+
+    public String getMD5Title() {
+        return MD5Title;
     }
 }
