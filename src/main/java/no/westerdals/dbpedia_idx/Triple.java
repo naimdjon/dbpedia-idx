561a3a67f9763e6958267ce0422d74647df334e9
naimdjon <naimdjon@gmail.com>
2015-10-14 23:42:13 +0200

    added categories.

--- src/main/java/no/westerdals/dbpedia_idx/Triple.java
+++ src/main/java/no/westerdals/dbpedia_idx/Triple.java
@@ -1,15 +1,30 @@
 package no.westerdals.dbpedia_idx;
 
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public final class Triple {
 
     public final String subject, predicate, property;
-    private final String MD5Title;
+    public final String MD5Title;
+    public final String subjectLowerCased;
+    private Set<Category> categories = new HashSet<>();
 
-    public Triple(String subject, String predicate, String property) {
+    public Triple(String ID, String propertyName, String property, Collection<Category> categories) {
+        this(ID, propertyName, property);
+        setCategories(categories);
+    }
+
+    public Triple(String subject, String propertyName, String property) {
         this.subject = subject;
-        this.predicate = predicate;
+        this.subjectLowerCased = subject.toLowerCase();
+        this.predicate = propertyName;
         this.property = property;
-        this.MD5Title=MD5.hash(this.subject);
+        this.MD5Title = MD5.hash(this.subject);
     }
 
     @Override
@@ -23,5 +38,30 @@ public final class Triple {
 
     public String getMD5Title() {
         return MD5Title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Triple entity = (Triple) o;
+        return Objects.equals(subject, entity.subject) &&
+                Objects.equals(predicate, entity.predicate) &&
+                Objects.equals(property, entity.property) &&
+                Objects.equals(MD5Title, entity.MD5Title) &&
+                Objects.equals(subjectLowerCased, entity.subjectLowerCased);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subject, predicate, property, MD5Title, subjectLowerCased);
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Collection<Category> categories) {
+        this.categories = ImmutableSet.copyOf(categories);
     }
 }
